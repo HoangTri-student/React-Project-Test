9d30397a8d0f641e17dd1589f63aa9499ac36f57
HoangTri-student <hoangtri.work@gmail.com>
2025-05-23 23:03:48 +0700

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -16,10 +16,10 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>Vite + React</h1>
+      <h1>Hoang Tri </h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
+          Click de dem {count}
         </button>
         <p>
           Edit <code>src/App.jsx</code> and save to test HMR
